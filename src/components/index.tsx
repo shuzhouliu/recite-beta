354a66b656455759f761a4cfc11b6316e4c07afb
shuzhouliu <shuzhou.liu@hotmail.com>
2022-07-18 22:01:19 +0800

    ci: make prettier happy

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,51 +8,55 @@ import React from "react";
 import poems from "./poems.tsx";
 
 type StateType = {
-    poem_text: string;
-    poem_origin: string;
+  poem_text: string;
+  poem_origin: string;
 };
 
 interface IndexPage {
-    state: StateType;
+  state: StateType;
 }
 
 class IndexPage extends React.Component {
-    state: StateType = {
-        poem_text: "",
-        poem_origin: ""
+  state: StateType = {
+    poem_text: "",
+    poem_origin: ""
+  };
+  mounted: boolean = false;
+
+  UNSAFE_componentWillMount() {
+    this.mounted = true;
+  }
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+  constructor(props: any) {
+    super(props);
+    if (this.mounted) {
+      var get_poem_attribute = props.get;
+      if (get_poem_attribute === "random") {
+        // rnd between [0, poems.length)
+        let rnd: number = Math.floor(Math.random() * poems.length);
+        this.setState({
+          poem_text: poems[rnd].poem_text,
+          poem_origin: poems[rnd].poem_origin
+        });
+      } else {
+        this.setState({
+          poem_text: poems[get_poem_attribute].poem_text,
+          poem_origin: poems[get_poem_attribute].poem_origin
+        });
+      }
     }
-    mounted: boolean = false;
-    
-    UNSAFE_componentWillMount() { this.mounted = true; }
-    componentWillUnmount() { this.mounted = false; }
-    constructor(props: any) {
-        super(props);
-        if (this.mounted) {
-            var get_poem_attribute = props.get;
-            if (get_poem_attribute === "random") {
-                // rnd between [0, poems.length)
-                let rnd: number = Math.floor(Math.random() * poems.length);
-                this.setState({
-                    poem_text: poems[rnd].poem_text,
-                    poem_origin: poems[rnd].poem_origin
-                });
-            } else {
-                this.setState({
-                    poem_text: poems[get_poem_attribute].poem_text,
-                    poem_origin: poems[get_poem_attribute].poem_origin
-                });
-            }
-        }
-    }
-    render(): React.ReactNode {
-        return (
-            <>
-                {this.state.poem_text}
-                <br />
-                ——{this.state.poem_origin}
-            </>
-        );
-    }
+  }
+  render(): React.ReactNode {
+    return (
+      <>
+        {this.state.poem_text}
+        <br />
+        ——{this.state.poem_origin}
+      </>
+    );
+  }
 }
 
 export { IndexPage };
