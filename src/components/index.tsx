0f789ce63fea697f9c51ec123507efb907ee9b12
shuzhouliu <shuzhou.liu@hotmail.com>
2022-07-18 22:09:56 +0800

    feat&chore: use yiyan api

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,8 +4,6 @@
  * Code is served under the MIT license.
  */
 import React from "react";
-// @ts-ignore
-import poems from "./poems.tsx";
 
 type StateType = {
   poem_text: string;
@@ -31,31 +29,23 @@ class IndexPage extends React.Component {
   }
   constructor(props: any) {
     super(props);
-    if (this.mounted) {
-      var get_poem_attribute = props.get;
-      if (get_poem_attribute === "random") {
-        // rnd between [0, poems.length)
-        let rnd: number = Math.floor(Math.random() * poems.length);
-        this.setState({
-          poem_text: poems[rnd].poem_text,
-          poem_origin: poems[rnd].poem_origin
+        fetch("https://v1.jinrishici.com/all.json", {
+            method: "GET"
+        }).then(res => res.json()).then(data => {
+            this.setState({
+                poem_text: data.content,
+                poem_origin: data.author+"《"+data.origin+"》"
+            });
         });
-      } else {
-        this.setState({
-          poem_text: poems[get_poem_attribute].poem_text,
-          poem_origin: poems[get_poem_attribute].poem_origin
-        });
-      }
-    }
   }
   render(): React.ReactNode {
-    return (
-      <>
-        {this.state.poem_text}
-        <br />
-        ——{this.state.poem_origin}
-      </>
-    );
+      return (
+        <>
+          {this.state.poem_text}
+          <br />
+          ——{this.state.poem_origin}
+        </>
+      );
   }
 }
 
