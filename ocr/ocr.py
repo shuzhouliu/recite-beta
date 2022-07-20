f5b185136283995ce709d0acb07624682083a830
Shuzhou Liu <64518651+shuzhouliu@users.noreply.github.com>
2022-07-20 20:33:16 +0800

    fix: ocr/ocr.py

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -5,6 +5,7 @@
 from paddleocr import PaddleOCR, draw_ocr
 import cv2
 import sys
+import io
 
 ocr = PaddleOCR (use_angle_cls=True, use_gpu=False)
 
@@ -19,13 +20,20 @@ else:
 
 result = ocr.ocr(img_path, cls=True)
 
-for line in result:
-    print(line)
 img = cv2.imread(img_path)
 boxes = [line[0] for line in result]
 txts = [line[1][0] for line in result]
 scores = [line[1][1] for line in result]
 
-img = draw_ocr(img, boxes, txts, scores)
-cv2.imshow("result",img)
-cv2.waitKey(0)
+str = ""
+
+for i in txts:
+    str += i
+
+with io.open('ocr.txt', 'w', encoding='utf-8') as f:
+    f.write(str)
+
+if (len(sys.argv) == 4 and sys.argv[3] == "--show-opencv"):
+    img = draw_ocr(img, boxes, txts, scores)
+    cv2.imshow("result",img)
+    cv2.waitKey(0)
